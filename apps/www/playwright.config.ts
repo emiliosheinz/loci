1aa2591d6aa802c7679ae7256bcd0a9d3fba7746
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-10-09 21:50:45 -0300

    test(www): add playwright e2e tests

---
+++ apps/www/playwright.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e-tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? "dot" : "list",
+  use: {
+    baseURL: "http://www:3001",
+    trace: "on-first-retry",
+    video: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    ...(process.env.CI
+      ? [
+        {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
+
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        },
+        {
+          name: "Mobile Chrome",
+          use: { ...devices["Pixel 5"] },
+        },
+        {
+          name: "Mobile Safari",
+          use: { ...devices["iPhone 12"] },
+        },
+      ]
+      : []),
+  ],
+});
