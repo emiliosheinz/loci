0b9f95bf862bbd120386fa634b9a9fdb0e51a8cc
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-10-31 18:09:40 -0300

    feat: replace nx with turborepo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? "localhost");
+
+  if (!process.env.PORT || !process.env.HOST) {
+    throw new Error("PORT and HOST must be defined in environment variables");
+  }
+
+  await app.listen(process.env.PORT, process.env.HOST, () => {
+    console.log(
+      `🚀 Server is running on http://${process.env.HOST}:${process.env.PORT}`,
+    );
+  });
 }
 bootstrap();
